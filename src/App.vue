b369bb78038d43b516ff563697663d84236e97fb
cinyearchan <cinyearchan@gmail.com>
2022-08-12 17:31:41 +0800

    feat(custom): 添加主路口

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,33 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="fc-designer-wrap">
+    <form-designer ref="fcDesignerRef">
+      <template #btns>
+        <el-button plain @click="printLog($refs.fcDesignerRef)">
+          打印JSON
+        </el-button>
+        <el-button plain @click="printOptions($refs.fcDesignerRef)">
+          打印Options
+        </el-button>
+      </template>
+    </form-designer>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import formDesigner from '@/packages/designer'
+
+const printLog = ref => {
+  console.log('rule', ref.getRule())
+}
+const printOptions = ref => {
+  console.log('option', ref.getOption())
+}
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.fc-designer-wrap {
+  height: 100vh;
 }
 </style>
